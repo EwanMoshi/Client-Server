938fa1ee850fc32b56f42e199d288f86d9017853
Ewan Moshi <ewanmoshi@live.com>
2022-03-22 17:16:12 +1100

    added UDPSocket class which wraps a udp socket

---
+++ Networking/NetworkingUtil/pch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#define NOMINMAX
+
+	#include "Windows.h"
+	#include "WinSock2.h"
+	#include "Ws2tcpip.h"
+	typedef int socklen_t;
+	
+#else
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <sys/types.h>
+	#include <netdb.h>
+	#include <errno.h>
+	#include <fcntl.h>
+	#include <unistd.h>
+	typedef int SOCKET;
+	const int NO_ERROR = 0;
+	const int INVALID_SOCKET = -1;
+	const int WSAECONNRESET = ECONNRESET;
+	const int WSAEWOULDBLOCK = EAGAIN;
+	const int SOCKET_ERROR = -1;
+#endif
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
